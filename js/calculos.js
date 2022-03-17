7ab6e9f35d1c326925f1b211c4c94e737b35bf12
AlvaroFigueroaG <alvaro.figueroa@gmx.com>
2022-03-17 11:03:37 +0100

    actualizacion

--- js/calculos.js
+++ js/calculos.js
@@ -6,16 +6,43 @@ function leerNumero() {
     return num;
 }
 
-function mostrarResultados() {
-    document.getElementById('td1').innerHTML = n;
-    document.getElementById('td2').innerHTML = factorial();
+function mostrarMultiplos(n, m) {
+    document.getElementById('td11').innerHTML = n;
+    document.getElementById('td12').innerHTML = m;
+    document.getElementById('td13').innerHTML = multiplosDeHasta(n, m);
 }
 
-function factorial() {
+
+
+function mostrarResultados(nf) {
+    document.getElementById('td21').innerHTML = nf;
+    document.getElementById('td22').innerHTML = factorial(nf);
+}
+
+function mostrarFactorial(nf) {
+    document.getElementById('td21').innerHTML = nf;
+    document.getElementById('td22').innerHTML = factorial(nf);
+}
+
+function factorial(nf) {
     var factorial = 1;
-    for ( i=1; i <= n; i++ ) {
+    for (i = 1; i <= nf; i++) {
         factorial = factorial * i;
     }
-
     return factorial;
 }
+
+function multiplosDeHasta() {
+     mensaje = "Los múltiplo de " +
+         n +
+         " hasta " + m +
+         " son: ";
+
+     multiplo = 0;
+     for (i = 1; multiplo < m; i++) {
+         multiplo = n * i;
+         mensaje = mensaje + multiplo + " ";
+     }
+
+     return mensaje;
+ }
